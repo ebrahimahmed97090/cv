e11983be86de8b49772cb60b282a8b244fa8de3c
Ebrahim Ahmed <ebrahimahmed97090@gmail.com>
2021-03-05 17:06:54 +0200

    lottie init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,29 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {ResumeComponent} from './resume/resume.component';
+import {SidenavComponent} from './sidenav/sidenav.component';
+import player from 'lottie-web';
+import {LottieModule} from 'ngx-lottie';
 
-@NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
-})
-export class AppModule { }
+export function playerFactory(): any {
+    return player;
+}
+
+@NgModule ({
+               declarations: [
+                   AppComponent,
+                   ResumeComponent,
+                   SidenavComponent
+               ],
+               imports     : [
+                   BrowserModule,
+                   AppRoutingModule,
+                   LottieModule.forRoot ({player: playerFactory}),
+               ],
+               providers   : [],
+               bootstrap   : [AppComponent]
+           })
+export class AppModule {}
